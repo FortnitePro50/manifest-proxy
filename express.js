a1ff77009b73afcc9ea526fe8238a3f533445105
FortnitePro50 <therealcalumyt@gmail.com>
2026-05-14 16:15:42 -0800

    Update express.js

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const SCRAPER_ORIGIN = 'https://cdn-bubbles.xyz';
 
 app.use(cors({
